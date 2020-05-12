5e4e787908b2762526911e697e249c01528e8723
Valentina Panic <valentina.panic87@gmail.com>
2020-05-12 04:19:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,10 +15,10 @@ end
 
  def take_a_number (katz_deli,name)
 
-  #index = 1
+  index = 1
   katz_deli.each do |name|
-   katz_deli << "Welcome #{name}. You are number #{katz_deli.length} in line."
-    #index +=1
+   katz_deli << "Welcome #{name}. You are number #{index} in line."
+    index +=1
 end
   puts line
 
