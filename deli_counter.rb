4b72bdb06cbe45031b84892c36cc874f8dd08fbc
Valentina Panic <valentina.panic87@gmail.com>
2020-05-12 03:36:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,9 +17,8 @@ end
 line = ""
   #counter = 1
   katz_deli.map.with_index do |name, index|
-    line.push("Welcome, #{name}. You are number #{index + 1} in line.")
+  puts  line.push("Welcome, #{name}. You are number #{index + 1} in line.")
     index +=1
 end
-puts line
 
 end
