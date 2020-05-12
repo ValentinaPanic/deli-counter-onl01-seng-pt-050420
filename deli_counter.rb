7472e5caf833abf6940f7a9dc9a0429d94d363ea
Valentina Panic <valentina.panic87@gmail.com>
2020-05-12 02:21:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def line(katz_deli)
+if katz_deli.length > 0
+  queue = "The line is currently:"
+   katz_deli.map.with_index do |name, index|
+  queue << " #{index +1}. #{name}"
+    index +=1
+  end
+  puts queue
+else katz_deli.length == 0
+  puts "The line is currently empty."
+end
+end
