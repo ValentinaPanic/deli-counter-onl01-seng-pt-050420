875f29916bbc469c0d8d34ebbc24e15ed18087de
Valentina Panic <valentina.panic87@gmail.com>
2020-05-12 04:43:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 end
 
  def take_a_number (katz_deli,name)
-  puts katz_deli << "Welcome #{name}. You are number #{katz_deli.length+1} in line."
+  puts "Welcome #{name}. You are number #{katz_deli.length+1} in line."
 end
