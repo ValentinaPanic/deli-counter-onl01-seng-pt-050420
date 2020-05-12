167bd91b9bd6a8bce367d549c03d0dd42891d884
Valentina Panic <valentina.panic87@gmail.com>
2020-05-12 04:38:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,9 +14,5 @@ end
 end
 
  def take_a_number (katz_deli,name)
-  #index = 1
-  #katz_deli.map do |name|
   puts katz_deli << "Welcome #{name}. You are number #{katz_deli.length+1} in line."
-    #index +=1
-#end
 end
